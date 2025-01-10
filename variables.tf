9d09d090c4c50ddfa6520e83fbab3eab0b874786
TK <tyagraj@gmail.com>
2025-01-10 11:33:54 +0530

    Create variables.tf
    variables.tf file for the project

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+}
+
+variable "organization_id" {
+  description = "Google Cloud organization ID"
+  type        = string
+}
